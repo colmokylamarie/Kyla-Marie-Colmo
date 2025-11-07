fceb79cfeea7f56ecbde917c39ef6e29bf17cdd8
colmokylamarie <colmokylamarie@isufst.edu.ph>
2025-11-07 12:10:40 +0800

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, jsonify, request 
-app = Flask(__name__) 
+from flask import Flask, jsonify
 
-@app.route('/') 
-def home(): 
-    return "Welcome to my Flask API!" 
-    
-@app.route('/student')
-def get_student(): 
-    return jsonify({ 
-        "name": "Your Name", 
-        "grade": 10, 
-        "section": "Zechariah" 
-    })
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to my Flask API!"
+
+@app.route('/student', methods=['GET'])
+def get_student():
+    student_info = {
+        "name": "Your Name",
+        "grade": 10,
+        "section": "Zechariah"
+    }
+    return jsonify(student_info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
